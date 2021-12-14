ebadffa7008abd50124d4317157eb23b821226b3
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2021-12-14 18:54:12 +0530

    typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,8 @@
+public class questions {
+    public static void main(String[] args) {
+       byte a=10;
+       byte b=20;
+       byte c=  (byte)(a+b) ;
+        System.out.println("value of c"+ c);
+    }
+}
